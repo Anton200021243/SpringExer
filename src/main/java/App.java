e48d1e8137608658dd608b472cf84493227f8ce6
Anton200021243 <mortal0011@gmail.com>
2024-10-18 19:14:15 +0300

    2.1.2 Spring

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,24 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("catBean");
+        System.out.println(beanCat1.getName());
+
+        Cat beanCat2 =
+                (Cat) applicationContext.getBean("catBean");
+        System.out.println(beanCat2.getName());
+
+        System.out.println(bean1 == bean2);
+        System.out.println(beanCat1 == beanCat2);
     }
 }
